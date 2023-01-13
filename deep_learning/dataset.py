0a68ae0d7090abf79ee68a4ab937443e9cee90ab
Steven Kang <kangweibaby@163.com>
2023-01-14 02:21:34 +0800

    04 Gradient

--- deep_learning/dataset.py
+++ deep_learning/dataset.py
@@ -2,17 +2,29 @@
 
 import os
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import pickle
+import numpy as np
+from collections import defaultdict
+
+
+dataset_path = 'datasets/minst.pickle'
 
 
 def load_mnist():
-    dataset_path = 'datasets/minst.pickle'
     if not os.path.exists(dataset_path):
-        mnist = datasets.fetch_openml('mnist_784', version=1, as_frame=False, parser='auto')
+        print("load mnist datasets from openml....")
+        origin = datasets.fetch_openml('mnist_784', version=1, as_frame=False, parser='auto')
         if not os.path.exists(os.path.dirname(dataset_path)):
             os.makedirs(os.path.dirname(dataset_path))
+        mnist = [
+            origin.data,
+            origin.target,
+        ]
         with open(dataset_path, 'wb') as file:
             file.write(pickle.dumps(mnist))
     else:
         with open(dataset_path, 'rb') as f:
             mnist = pickle.load(f)
+
+    return train_test_split(mnist[0], mnist[1], test_size=0.25)
